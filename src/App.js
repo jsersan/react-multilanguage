7fcda81d46d0392f6c7556120452814962038002
Txema Serrano <jsersan@gmail.com>
2022-08-01 19:46:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import i18n from './i18n';
+import { withNamespaces } from 'react-i18next';
 
-function App() {
+function App ({ t }) {
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="idiomas">
+      <button class="btn btn-link" onClick={() => changeLanguage('de')}>de</button>
+      <button class="btn btn-link" onClick={() => changeLanguage('en')}>en</button>
+      <button class="btn btn-link" onClick={() => changeLanguage('es')}>es</button>
+      <button class="btn btn-link" onClick={() => changeLanguage('eu')}>eu</button> 
+      <h1>{t('Welcome to React')}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default withNamespaces()(App);
